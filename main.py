33edbfe9916550b90fd33fd65d76c5e587a78a36
XQluziff <xqluziff@yandex.ru>
2023-03-16 12:00:12 +0300

    HomeWork 2

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-
+monets = int(input('Количество монет'))
+count_orel = 0
+count_reshko = 0
+for _ in range(monets):
+    x = int(input('Орел-1 / Решко-0'))
+    if (x == 1):
+        count_orel += 1
+    else:
+        count_reshko += 1
+print()
 
